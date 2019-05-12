78e19ea9908d460ebf74ce2bd2b0b02948a6ae84
b8hri11 <50506825+b8hri11@users.noreply.github.com>
2019-05-12 23:54:57 +0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,13 @@
+DMD library origin
+--------------
+Marc Alexander, Freetronics
+Email: info (at) freetronics.com
+URL:   http://www.freetronics.com/dmd-library
+
+Adapted for STM32 
+-------------
+mozokevgen
+
+To Do:
+  - Fix Examples
+  - Clean code 
---
+++ examples/dmd_latin_chars/dmd_latin_chars.ino
@@ -0,0 +1,103 @@
+/*--------------------------------------------------------------------------------------
+
+ dmd_latin_chars
+   Demo and example Latin-1 encoding project for the Freetronics DMD, a 512 LED matrix display
+   panel arranged in a 32 x 16 layout.
+
+See http://www.freetronics.com/dmd for resources and a getting started guide.
+
+This example code is in the public domain. It demonstrates using the ISO-8859-1
+(Latin-1) extended character set. Thanks to Xavier Seignard for contributing
+Latin-1 support.
+
+*******************************
+HOW TO ENTER Latin-1 CHARACTERS
+*******************************
+
+Unfortunately entering Latin-1 characters like Ã or è is not as simple as just
+typing them. Arduino Sketches are saved in Unicode UTF-8 format, but the DMD library
+does not understand Unicode (it's too complex.)
+
+To enter the characters as Latin-1, look at the codepage layout here and look for
+the hexadecimal digit that corresponds to the character you want.
+https://en.wikipedia.org/wiki/ISO/IEC_8859-1#Codepage_layout
+
+For example, á (lowercase a, rising diacritical mark) has hex value 00E0 in Latin-1.
+To translate this into a constant string, replace the leading 00 with \x - so the
+string could be "The Portugese for rapid is r\xE0pido".
+
+To be safe, the string may also need to be separated in its own quote marks - ie
+"The Portugese for rapid is r""\xE0""pido"
+
+When you compile the sketch, the compiler will join all these strings up into one
+long string - however without the quotes around the \x it may try to include additional
+characters as part of the hexadecimal sequence.
+
+/*--------------------------------------------------------------------------------------
+  Includes
+--------------------------------------------------------------------------------------*/
+#include <SPI.h>        //SPI.h must be included as DMD is written by SPI (the IDE complains otherwise)
+#include <DMDSTM.h>        //
+//#include <TimerOne.h>   //
+#include "SystemFont5x7.h"
+#include "Arial_Black_16_ISO_8859_1.h"
+SPIClass SPI_2(2);
+
+//Fire up the DMD library as dmd
+#define DISPLAYS_ACROSS 1
+#define DISPLAYS_DOWN 1
+DMD dmd(DISPLAYS_ACROSS, DISPLAYS_DOWN);
+
+/*--------------------------------------------------------------------------------------
+  Interrupt handler for Timer1 (TimerOne) driven DMD refresh scanning, this gets
+  called at the period set in Timer1.initialize();
+--------------------------------------------------------------------------------------*/
+void ScanDMD()
+{ 
+  dmd.scanDisplayBySPI(SPI_2);
+}
+
+/*--------------------------------------------------------------------------------------
+  setup
+  Called by the Arduino architecture before the main loop begins
+--------------------------------------------------------------------------------------*/
+void setup(void)
+{
+	SPI_2.begin();                          //Initialize the SPI_2 port.
+    SPI_2.setBitOrder(MSBFIRST);            // Set the SPI_2 bit order
+    SPI_2.setDataMode(SPI_MODE0);           //Set the  SPI_2 data mode 0
+    SPI_2.setClockDivider(SPI_CLOCK_DIV64); // Use a different speed to SPI 1
+    SPI.setModule(2);
+    pinMode(SPI2_NSS_PIN, OUTPUT);
+    //clear/init the DMD pixels held in RAM
+    dmd.clearScreen( true );   //true is normal (all pixels off), false is negative (all pixels on)
+    //  Led Panel setup
+    Timer3.setMode(TIMER_CH1, TIMER_OUTPUTCOMPARE);
+    Timer3.setPeriod(3000);          // in microseconds
+    Timer3.setCompare(TIMER_CH1, 1); // overflow might be small
+    Timer3.attachInterrupt(TIMER_CH1, ScanDMD);
+	dmd.setBrightness(100);
+  Serial.begin(115200);
+}
+
+/*--------------------------------------------------------------------------------------
+  loop
+  Arduino architecture main loop
+--------------------------------------------------------------------------------------*/
+void loop(void)
+{
+   dmd.clearScreen( true );
+   dmd.selectFont(Arial_Black_16_ISO_8859_1);
+   // Français, Österreich, Magyarország
+   const char *MSG = "Fran""\xe7""ais, ""\xd6""sterreich, Magyarorsz""\xe1""g";
+   dmd.drawMarquee(MSG,strlen(MSG),(32*DISPLAYS_ACROSS)-1,0);
+   long start=millis();
+   long timer=start;
+   while(1){
+     if ((timer+30) < millis()) {
+       dmd.stepMarquee(-1,0);
+       timer=millis();
+     }
+   }
+}
+
